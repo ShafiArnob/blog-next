4e25b0bfd4c6817fc9762c3fdbc6a06af5762129
ShafiArnob <shafiarnob@gmail.com>
2024-03-24 05:44:29 +0600

    hono -> express init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,46 @@
-import { Hono } from "hono";
 
-const app = new Hono();
 
-app.post("/api/v1/user/signup", (c) => {
-  return c.text("Hello Hono!");
-});
 
-app.post("/api/v1/user/signin", (c) => {
-  return c.text("Hello Hono!");
-});
 
-app.put("/api/v1/user/blog", (c) => {
-  return c.text("Hello Hono!");
-});
 
-app.get("/api/v1/user/blog/:id", (c) => {
-  return c.text("Hello Hono!");
-});
+// app.post("/api/v1/user/signup", async(c) => {
+//   const prisma = new PrismaClient({
+//     // @ts-ignore
+//     datasources: c.env.DATABASE_URL
+//   })
+//   const body = await c.req.json()
+  
+//   try{
+//     await prisma.user.create({
+//       data:{
+//         email: body.username,
+//         password: body.password,
+//         name: body.name
+//       }
+//     })
+//     return c.text("Hello Hono!");
+//   }
+//   catch(e){
+//     console.log(e);
+    
+//     return c.text("Invalid");
+//   } 
+// });
 
-app.get("/api/v1/user/blog/bulk", (c) => {
-  return c.text("Hello Hono!");
-});
+// app.post("/api/v1/user/signin", (c) => {
+//   return c.text("Hello Hono!");
+// });
 
-export default app;
+// app.put("/api/v1/user/blog", (c) => {
+//   return c.text("Hello Hono!");
+// });
+
+// app.get("/api/v1/user/blog/:id", (c) => {
+//   return c.text("Hello Hono!");
+// });
+
+// app.get("/api/v1/user/blog/bulk", (c) => {
+//   return c.text("Hello Hono!");
+// });
+
+// export default app;
